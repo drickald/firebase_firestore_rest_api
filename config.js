2d3e84c9c83fedd81e35e529ba11ba912f78542b
Camile Caragay <camsymile@gmail.com>
2025-12-27 22:34:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
  * Contains the Firebase API credentials required to connect to the REST API.
  */
 export const CONFIG = {
-    API_KEY: "AIzaSyAWEPNXEiUU4nh9WFioC5XrcTuptc2ZE2s",      // Found in Firebase Project Settings
+    API_KEY: "YOUR API KEY HERE",      // Found in Firebase Project Settings
     PROJECT_ID: "elec3-project",        // Found in Firebase Project Settings
 };
